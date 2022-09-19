2865f6973e1a9655047d736bcaef5f3ae435a92f
Eloka Ogbonnia <elokakene@gmail.com>
2022-09-19 23:05:40 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -36,3 +36,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+let cards = document.querySelectorAll('.cards');
+let container = document.querySelector('.deck');
+container.innerHTML = '';
+shuffledCards = shuffle(Array.from(cards));
+
+for (card of shuffledCards) {
+    container.appendChild(card);
+}
